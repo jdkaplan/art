9ddaad161c5ce7b7db9b9fc523fbe0f3e214449c
Rodrigo Toste Gomes <rgomes@codeblurb.com>
2022-04-30 22:17:43 -0700

    Rule 110!

---
+++ examples/rule_prettyprint.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import stdin
+
+i = 0
+for line in stdin:
+    extra_offset = 4 if (i // (8*30))%2 == 1 else 0
+    if (i - 2 - extra_offset) % (8*30) != 0:
+        i += 1
+        continue
+    print(line.strip().replace('O', '0').replace('I', '|'))
+
+    i += 1
---
+++ examples/rule_prettyprint.sh
@@ -0,0 +1,1 @@
+../main.py rule.art -c none -t 0 --no-clear --no-iteration | python rule_prettyprint.py
